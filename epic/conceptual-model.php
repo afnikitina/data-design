0602d7bc763d023782ce4f1d7e518c86df60a51c
Asya Nikitina <asya_nikitina@hotmail.com>
2018-07-14 19:48:14 -0600

    Modified conceptual-model.php. Added edr1.jpg and er5.jpg to the project.

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -37,7 +37,7 @@
 				</ul>
 			</div>
 			<div>
-				<h3>Comment (Old Design)</h3>
+				<h3>Comment</h3>
 				<ul>
 					<li><span>commentId (Primary Key)</span></li>
 					<li>commentArticleId (Foreign Key)</li>
@@ -47,15 +47,32 @@
 				</ul>
 			</div>
 			<div>
+				<h3>Entity Relationship Diagram Using Crow’s Foot Notation</h3>
+				<img src="../images/erd1.jpg" alt="Crow’s Foot Notation">
+			</div>
+			<div>
+				<h3>Entity Relationship Diagram Using Intersection Tables</h3>
+				<img src="../images/erd5.jpg" alt="ERD Using Intersection Tables">
+			</div>
+			<div>
 				<h3>Relations</h3>
 				<ul>
 					<li>Many different users can post multiple comments about many different articles <code>(n-to-m)</code></li>
+					<li>Each user can post from none to many comments.</li>
+					<li>Each article can have from none to many comments about it.</li>
+					<li>Each comment references only one user who posted it and only one article, which this comment is about.</li>
+				</ul>
+			</div>
+			<div>
+				<h3>Assumption</h3>
+				<ul>
+					<li>A user can post multiple comments about the same article (multiple instances of the <strong>Comment</strong> entity may have exactly the same combination of their foreign keys).</li>
 				</ul>
 			</div>
 			<div>
 				<h3>Legend</h3>
 				<ul>
-					<li><span>Primary Key/Composite Primary Key</span></li>
+					<li><span>Primary Key</span></li>
 				</ul>
 			</div>
 		</main>
