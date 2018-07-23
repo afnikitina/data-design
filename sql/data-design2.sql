26572b399e19985ba69f95a693e66fabf8534de1
Asya Nikitina <asya_nikitina@hotmail.com>
2018-07-22 22:00:55 -0600

    Modified data-design2.sql

--- sql/data-design2.sql
+++ sql/data-design2.sql
@@ -2,12 +2,12 @@ USE anikitina;
 -- set UTF-8 charset
 ALTER DATABASE anikitina CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS articles;
-DROP TABLE IF EXISTS comments
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS article;
+DROP TABLE IF EXISTS comment;
 
 -- create the user entity
-CREATE TABLE users (
+CREATE TABLE user (
 	userId BINARY(16) NOT NULL,
 	userEmail VARCHAR(128) NOT NULL,
 	userHash CHAR(97) NOT NULL,
@@ -20,7 +20,7 @@ CREATE TABLE users (
 );
 
 -- create the article entity
-CREATE TABLE articles (
+CREATE TABLE article (
 	articleId BINARY(16) NOT NULL,
 	articleAuthor VARCHAR(92) NOT NULL,
 	articleCategory VARCHAR(20) NOT NULL,
@@ -28,21 +28,21 @@ CREATE TABLE articles (
 	articleDate DATE NOT NULL,
 	articleTitle VARCHAR(92),
 	INDEX(articleAuthor(20)),
-	INDEX(artiarticleTitlecleCategory(4)),
-	INDEX(articleTitle(20)),
+	INDEX(articleCategory(4)),
 	INDEX(articleDate),
+	INDEX(articleTitle(20)),
 	PRIMARY KEY(articleId)
 );
 
 -- create the comment entity
-CREATE TABLE comments (
+CREATE TABLE comment (
 	commentId BINARY(16) NOT NULL,
 	commentArticleId BINARY(16) NOT NULL,
 	commentUserId BINARY(16) NOT NULL,
 	commentContent VARCHAR(250),
 	commentDate DATE NOT NULL,
 	INDEX(commentDate),
-	FOREIGN KEY(commentArticleId) REFERENCES articles(articleId),
-	FOREIGN KEY(commentUserId) REFERENCES users(userId),
+	FOREIGN KEY(commentArticleId) REFERENCES article(articleId),
+	FOREIGN KEY(commentUserId) REFERENCES user(userId),
 	PRIMARY KEY(commentId)
 );
